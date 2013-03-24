625d2f77649dd60c8df2a86c718359a49df56b70
Maciej Szewczyk <maciej.w.szewczyk@gmail.com>
2013-03-24 10:29:07 +0100

    ranamed

--- src/smoke11/DebugView.java
+++ src/smoke11/DebugView.java
@@ -11,14 +11,13 @@ import java.io.PrintStream;
  * Time: 09:55
  * To change this template use File | Settings | File Templates.
  */
-public class DebugViewPanel {
-    private static JFrame window;
+public class DebugView {
     private static int debugLevel;//0-no debug, 1-errors, 2-most important info, 3-less and etc.
     public static final int DEBUGLVL_ERRORS = 1;
     public static final int DEBUGLVL_LESSINFO = 2;
     public static final int DEBUGLVL_MOREINFO = 3;
 
-    public static void createWindow(int locX, int locY, int sizeX, int sizeY, int debuglevel)
+    public static JFrame createWindow(int locX, int locY, int sizeX, int sizeY, int debuglevel)
     {
         //Create and set up the debug window.
         debugLevel=debuglevel;
@@ -28,7 +27,7 @@ public class DebugViewPanel {
         PrintStream ps = new PrintStream( taos );
         System.setOut( ps );
         System.setErr( ps );
-        window = new JFrame ("Debug Console");
+        JFrame window = new JFrame ("Debug Console");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JScrollPane jpane = new JScrollPane( ta );
         jpane.setSize(sizeX-100,sizeY);
@@ -39,6 +38,27 @@ public class DebugViewPanel {
         System.out.println("Initializing Debug Window.");
         System.out.println("Showing messages for debug level == "+ debugLevel);
         System.out.println("------");
+        return window;
+    }
+    public static JPanel createPanel(int sizeX, int sizeY, int debuglevel)
+    {
+        //Create and set up the debug window.
+        debugLevel=debuglevel;
+        JTextArea ta = new JTextArea();
+        ta.setSize(sizeX-100,sizeY);
+        TextAreaOutputStream taos = new TextAreaOutputStream( ta, Integer.MAX_VALUE );
+        PrintStream ps = new PrintStream( taos );
+        System.setOut( ps );
+        System.setErr( ps );
+        JScrollPane jpane = new JScrollPane( ta );
+        jpane.setSize(sizeX-100,sizeY);
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(sizeX,sizeY));
+        panel.add(jpane);
+        System.out.println("Initializing Debug Window.");
+        System.out.println("Showing messages for debug level == "+ debugLevel);
+        System.out.println("------");
+        return panel;
     }
     public static void writeDebug(int level, String classname, String message)
     {
